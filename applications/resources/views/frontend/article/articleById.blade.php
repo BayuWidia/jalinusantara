ed562a59520cd765b862f0659bfe57eb3aa313cd
devguy <gunawan.prasetio@ramayana.co.id>
2019-06-13 16:44:59 +0700

    test

--- applications/resources/views/frontend/article/articleById.blade.php
+++ applications/resources/views/frontend/article/articleById.blade.php
@@ -72,7 +72,6 @@
             <!-- Go to www.addthis.com/dashboard to customize your tools -->
             <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-58a5140167663aee"></script>
             <div class="addthis_inline_share_toolbox"></div>
-
 					</div>
 				</div>
 
